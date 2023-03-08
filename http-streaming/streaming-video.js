2f03d7476527e162194abfdee9cba5d501a6cd03
samroodAli <devsamrood@gmail.com>
2023-03-08 12:06:31 +0530

    feat: support range requests from html5 videos timestamp jumps

--- http-streaming/streaming-video.js
+++ http-streaming/streaming-video.js
@@ -15,10 +15,36 @@ async function handleRequest(req, res) {
   // stat gives data about the file such as size of the file
   const { size } = await stat(videoPath);
 
-  res.writeHead(200, {
-    "Content-Type": "video/mp4",
-    "Content-Length": size,
-  });
+  // handling range requests
+  // range requests are when we click on different timestamps in the video
+  // i.e, jumping to different parts of the video makes the browser send a request with the header 'range'
+  // the 'range' header is in the format `bytes=${startingRange}-${endingRange}' where
+  // starting range is always a number, but endingRange might not be present (empty string)
+  //
 
-  createReadStream(videoPath).pipe(res);
+  const range = req.headers.range;
+
+  if (range) {
+    let [start, end] = range.replace(/bytes=/, "").split("-");
+
+    start = parseInt(start, 10);
+
+    end = end ? parseInt(end, 10) : size - 1; // size - 1 because zero indexed
+
+    res.writeHead(206, {
+      "Content-Type": "video/mp4",
+      "Content-Length": end - start + 1, // + 1 because it's zero indexed
+      "Accept-Ranges": "bytes",
+      "Content-Range": `bytes ${start} - ${end}/${size}`,
+    });
+
+    createReadStream(videoPath, { start, end }).pipe(res);
+  } else {
+    res.writeHead(200, {
+      "Content-Type": "video/mp4",
+      "Content-Length": size,
+    });
+
+    createReadStream(videoPath).pipe(res);
+  }
 }
