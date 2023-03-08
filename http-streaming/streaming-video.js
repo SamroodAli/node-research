52c1ad7dfee889705107323be99ca5f82994f2b4
samroodAli <devsamrood@gmail.com>
2023-03-08 12:26:12 +0530

    feat: add forking streams and file upload

--- http-streaming/streaming-video.js
+++ http-streaming/streaming-video.js
@@ -1,17 +1,30 @@
 const { createServer } = require("http");
 const { stat } = require("fs/promises");
-const { createReadStream } = require("fs");
+const { createReadStream, createWriteStream } = require("fs");
 const path = require("path");
 
 const server = createServer(handleRequest);
 
 const videoPath = path.join(__dirname, "../demo-files/powder-day.mp4");
 
+const uploadPath = (file) =>
+  path.join(__dirname, `../ignored-demo-files/${file}`);
+
 server.listen(3000, () => {
   console.log("server listening on http://localhost:3000");
 });
 
 async function handleRequest(req, res) {
+  if (req.url === "/video") {
+    handleVideo(req, res);
+  } else if (req.url === "/upload" && req.method === "POST") {
+    handleFileUpload(req, res);
+  } else {
+    handleMultiPartFrom(req, res);
+  }
+}
+
+async function handleVideo(req, res) {
   // stat gives data about the file such as size of the file
   const { size } = await stat(videoPath);
 
@@ -47,4 +60,24 @@ async function handleRequest(req, res) {
 
     createReadStream(videoPath).pipe(res);
   }
+}
+
+async function handleMultiPartFrom(req, res) {
+  res.writeHead(200, {
+    "Content-Type": "text/html",
+  });
+
+  res.end(`
+    <form enctype="multipart/form-data" method="POST" action="/upload">
+      <input type="file" name="upload-file"/>
+      <button type="submit">Upload file</button>
+    </form>
+  `);
+}
+
+async function handleFileUpload(req, res) {
+  // forking streams
+  req.pipe(res);
+  req.pipe(process.stdout);
+  req.pipe(createWriteStream(uploadPath("someFile")));
 }
